6bfcc36da2a0dfbd7b38067d088fbe097ec4ed93
Dominic Fallows <consultant@dominicfallows.uk>
2015-02-18 19:20:33 +0000

    Updated folder structure ready for future additional projects

--- RomanArabicConverterConsoleApplication/Properties/AssemblyInfo.cs
+++ RomanArabicConverterConsoleApplication/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("Roman Arabic Converter Console Application")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Roman Arabic Converter Console Application")]
+[assembly: AssemblyCopyright("Copyright © Dominic Fallows 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
